053b4cf3c7199b195d86f5654043c692b6da1690
Robert Pate <robert.pate@gmail.com>
2017-10-08 16:14:35 -0500

    Remove windows-only build system
    Ant is for Java and is IDE/platform agnostic so I would rather put effort
    into it than a windows-only batch file that has no awareness of java.

--- hw2/build.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO Rebuilding...
-IF EXIST build rmdir /s /q build
-SET compile=javac
-IF NOT "%JAVA_HOME%"=="" SET compile=%JAVA_HOME%\bin\javac.exe
-mkdir build
-%compile% -d build Client.java
-%compile% -d build Server.java
