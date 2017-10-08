45771e54e586b2ac9a8d9282087f65d9aa0c2607
Robert Pate <robert.pate@gmail.com>
2017-10-08 17:36:02 -0500

    Parse server input for client

--- hw2/Client.java
+++ hw2/Client.java
@@ -1,34 +1,61 @@
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.net.*;
 
 public class Client {
+  static Socket currentServer;
+  static ArrayList<InetSocketAddress> servers;
   public static void main (String[] args) {
 
     Scanner sc = new Scanner(System.in);
     int numServer = sc.nextInt();
+    servers = new ArrayList<>();
 
     for (int i = 0; i < numServer; i++) {
-      // TODO: parse inputs to get the ips and ports of servers
+        String temp = sc.next();
+        int spacerIndex = temp.indexOf(":");
+        String host = temp.substring(0, spacerIndex);
+        int port = Integer.parseInt(temp.substring(spacerIndex + 1));
+        servers.add(new InetSocketAddress(host, port));
+    }
+
+    if (connectToServer() == false) {
+        System.out.println("ERROR: Could not connect to any servers");
+        return;
     }
 
     while(sc.hasNextLine()) {
       String cmd = sc.nextLine();
       String[] tokens = cmd.split(" ");
 
-      if (tokens[0].equals("reserve")) {
         // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("bookSeat")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else if (tokens[0].equals("delete")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-      } else {
-        System.out.println("ERROR: No such command");
-      }
+        // appropriate responses form the server for each case
+        switch (tokens[0]) {
+            case "reserve":
+                break;
+            case "bookSeat":
+                break;
+            case "search":
+                break;
+            case "delete":
+                break;
+            default:
+                System.out.println("ERROR: No such command");
+                break;
+        }
     }
   }
+
+  private static boolean connectToServer() {
+      return false;
+      // for each server in servers
+        // open socket new Socket(address.getAddress, address.getPort
+        // if success, exit loop
+        // if fail, remove server from list (per garg)
+        // close on error
+        // return false if all servers fail
+        // don't forget to close at end of main too
+  }
+
 }
