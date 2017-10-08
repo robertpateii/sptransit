d2c262aa0e12b4a90518c54f2418475308b70388
Robert Pate <robert.pate@gmail.com>
2017-10-08 01:59:56 -0500

    Fix run.cmd to sort of work for client

--- hw2/run.cmd
+++ hw2/run.cmd
@@ -1,4 +1,3 @@
-REM commands from HW1... won't work for 2 need to be multiple lines input
-REM start cmd /k java Server 3003 3004 inventory.txt
-REM start cmd /k java Client localhost 3003 3004
-REM start cmd /k java Client localhost 3003 3004
+java -cp .\build Client < client1.txt
+java -cp .\build Client < client2.txt
+pause
