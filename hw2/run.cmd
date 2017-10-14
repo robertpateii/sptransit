a91009231725b5c3d1ed80c2c4211b73b5a41404
Robert Pate <robert.pate@gmail.com>
2017-10-13 20:47:01 -0500

    Delay running stuff to match assumptions
    Assumption: one server is always up

--- hw2/run.cmd
+++ hw2/run.cmd
@@ -1,4 +1,6 @@
 start cmd /k "java -cp build Server < inputs\server1.txt"
+TIMEOUT 5
 start cmd /k "java -cp build Server < inputs\server2.txt"
+TIMEOUT 5
 start cmd /k "java -cp build Client < inputs\client1.txt"
 start cmd /k "java -cp build Client < inputs\client2.txt"
