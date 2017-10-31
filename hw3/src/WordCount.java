d308f7da726666724d920f42cd1e7e542225c937
Samuel Cherinet <samqty2005@gmail.com>
2017-10-31 15:13:36 -0700

    a working implementation of the assignment

--- hw3/src/WordCount.java
+++ hw3/src/WordCount.java
@@ -11,14 +11,20 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
 
 public class WordCount {
 
   public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+       extends Mapper<Object, Text, Text, Text>{
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
+    private Text file = new Text();
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
@@ -28,25 +34,45 @@ public class WordCount {
       simplified = simplified.replaceAll("[^a-z]"," ");
       StringTokenizer itr = new StringTokenizer(simplified);
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken() + "-" + fileName );
-        context.write(word, one);
+        word.set(itr.nextToken());
+	file.set(fileName);
+        context.write(word, file);
       }
     }
   }
 
   public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+       extends Reducer<Text,Text,Text,Text> {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
+    public void reduce(Text key, Iterable<Text> values,
                        Context context
                        ) throws IOException, InterruptedException {
       int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+	Map<String,Integer> fileCounts = new HashMap<String,Integer>();
+
+      for (Text val : values) {
+	if(fileCounts.containsKey(val.toString())){
+		fileCounts.put(val.toString(),fileCounts.get(val.toString())+1);
+	}
+	else{
+		fileCounts.put(val.toString(),1);
+	}
       }
-      result.set(sum);
-      context.write(key, result);
+
+	Map<String,Integer> sortedFileCounts = fileCounts.entrySet().stream()
+		.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+		.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+		(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+ 	String resultString = "";
+
+	for(String fileCount : sortedFileCounts.keySet())
+	{
+		resultString  += "\n< "+fileCount+", "+ sortedFileCounts.get(fileCount)+">";
+	}
+
+      context.write(key, new Text(resultString));
     }
   }
 
@@ -55,10 +81,10 @@ public class WordCount {
     Job job = Job.getInstance(conf, "word count");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
+    //job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
