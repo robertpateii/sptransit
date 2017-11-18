9cde8af4ece4cddd8de5bfd66cf24aa07d2278b7
Robert Pate <robert.pate@gmail.com>
2017-11-18 15:22:40 -0600

    Copy Samuel's changes from jtransit to sptransit

--- sptransit/src/sptransit/TMessage.java
+++ sptransit/src/sptransit/TMessage.java
@@ -1,5 +1,15 @@
 package sptransit;
 
 public class TMessage<E> {
-    public E Body;
+    private E _body;
+
+    public  TMessage(E body)
+    {
+        _body = body;
+    }
+
+    public E getBody(){return _body;}
+
+    public void setBody(E body){_body=body;}
 }
+
