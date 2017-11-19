35b085ac8ca2381645285fcdf91b627be44c145d
Sam Cherinet <samqty2005@gmail.com>
2017-11-18 23:07:26 -0600

    restructured send and receive, and added queue to socket

--- sptransit/src/sptransit/TMessage.java
+++ sptransit/src/sptransit/TMessage.java
@@ -2,6 +2,8 @@ package sptransit;
 
 public class TMessage<E> {
     private E _body;
+    private String _ipAddress;
+    private int _port;
 
     public  TMessage(E body)
     {
@@ -11,5 +13,20 @@ public class TMessage<E> {
     public E getBody(){return _body;}
 
     public void setBody(E body){_body=body;}
+
+    public void setSourceAddress(String ipAddress,int port){
+        _ipAddress = ipAddress;
+        _port = port;
+    }
+
+    public String getIpAddress()
+    {
+        return _ipAddress;
+    }
+
+    public int getPort()
+    {
+        return _port;
+    }
 }
 
--- sptransit/src/sptransit/TSocket.java
+++ sptransit/src/sptransit/TSocket.java
@@ -5,44 +5,85 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.SynchronousQueue;
 
 public class TSocket {
     private TContext _TContext;
-    private String _bindEndPoint;
-    private String _connectEndPoint;
+    private String _bindEndPointHost;
+    private int _bindEndPointPort;
+    private String _connectEndPointHost;
+    private int _connectEndPointPort;
+    private Runnable _serverRunner;
+    private SynchronousQueue<TMessage> _messageQueue;
 
-    public  TSocket(TContext jtcontext)
-    {
-        _TContext = jtcontext;
+    public TSocket(TContext tcontext) {
+        _TContext = tcontext;
+        _messageQueue = new SynchronousQueue<>();
     }
 
-    public void bind(String endpoint) {
-        _bindEndPoint = endpoint;
+    public void bind(String host, int port) {
+        _bindEndPointHost = host;
+        _bindEndPointPort = port;
+
+        _serverRunner = new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(port);
+
+                    while (true) {
+                        Socket clientSocket = serverSocket.accept();
+                        //TODO : this should create another thread not to block the server thread
+                        ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+                        PrintWriter out =
+                                new PrintWriter(clientSocket.getOutputStream(), true);
+                        TMessage messge = null;
+                        try {
+                            messge = (TMessage) in.readObject();
+
+                            //TODO: push it to the message queue
+                            _messageQueue.put(messge);
+
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        out.write("ACK");
+                        out.flush();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    System.err.println("Accept failed : ");
+                }
+            }
+        };
+
+        _serverRunner.run();
     }
 
-    public void connect(String endpoint){
-        _connectEndPoint = endpoint;
-        //todo : just connect and keep listening to messages here ?
+    public void connect(String host, int port) {
+        _connectEndPointHost = host;
+        _connectEndPointPort = port;
     }
 
     public void Send(TMessage message) {
-        //open a the tcp
-        Pattern pattern = Pattern.compile("");
-        Matcher matcher = pattern.matcher(_connectEndPoint);
-        matcher.find();
+        Send(message, _connectEndPointHost, _connectEndPointPort);
+    }
 
-        String host = matcher.group(1);
-        int port = Integer.parseInt(matcher.group(2));
-
+    public void Send(TMessage message, String host, int port) {
         InetAddress ia;
         try {
             ia = InetAddress.getByName(host);
-
             Socket s = new Socket(ia, port);
+
             BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
             ObjectOutputStream pout = new ObjectOutputStream(s.getOutputStream());
+
+            //set the source of the message here?
+            message.setSourceAddress(InetAddress.getLocalHost().toString(),_bindEndPointPort);
 
             pout.writeObject(message);
             pout.flush();
@@ -59,43 +100,7 @@ public class TSocket {
         }
     }
 
-    public TMessage Receive(){
-
-        try{
-            //todo : validate that the end point address has been initialized
-            Pattern pattern = Pattern.compile("");
-            Matcher matcher = pattern.matcher(_bindEndPoint);
-            matcher.find();
-
-            String host = matcher.group(1);
-            int port = Integer.parseInt(matcher.group(2));
-
-            System.out.println("TCP server starting");
-            ServerSocket listener = new ServerSocket(port);
-            Socket s;
-            while((s = listener.accept())!= null)
-            {
-                PrintWriter out =
-                        new PrintWriter(s.getOutputStream(), true);
-                ObjectInputStream in = new ObjectInputStream (s.getInputStream());
-
-                TMessage messge = null;
-                try {
-                    messge = (TMessage) in.readObject();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                out.write("ACK");
-                out.flush();
-                s.close();
-
-                return messge;
-            }
-        } catch(IOException e)
-        {
-            System.err.print(e);
-        }
-
-        return null;
+    public TMessage Receive() {
+        return _messageQueue.poll();
     }
 }
