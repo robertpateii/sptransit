7fab7d1f2e84b76082d2278b15d57251705e561e
Robert Pate <robert.pate@gmail.com>
2017-11-19 13:07:03 -0600

    Add client work in progress for helloworld

--- sptransit/samples/src/hello/Client.java
+++ sptransit/samples/src/hello/Client.java
@@ -5,5 +5,9 @@ public class Client {
     public static void main (String[] args) {
       TContext context = new TContext();
       TMessage msg = new TMessage<String>("Hello");
+      TSocket server = new TSocket(context);
+      server.bind("localhost",8000);
+      server.send(msg);
+
     }
 }
