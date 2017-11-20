7b2ca7d852f9ef1d0dfc0cb803667a010ef37b76
Robert Pate <robert.pate@gmail.com>
2017-11-19 18:01:46 -0600

    Add a test script for hello that doesn't work

---
+++ sptransit/samples/hello-test.ps1
@@ -0,0 +1,4 @@
+echo "Warning this doesn't work because the pathing is wrong either for java or logger"
+echo "Running server and client in intellij works fine though"
+# start cmd "/k java -cp out\production\samples hello.Server"
+# start cmd "/k java -cp out\production\samples hello.Client"
